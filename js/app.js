71e591b11ecfe6ec50b137d0e63589da92888bf9
Eurys Corrales <eurysc@gmail.com>
2026-08-08 21:13:17 -0500

    v0.6.0-beta1 Dashboard Ejecutivo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  console.log('HorizonOS Project Family Budget Base');
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('HorizonOS Project Family Budget v0.6.0-beta1');
 });
